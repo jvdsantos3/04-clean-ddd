d53c9ea878f479c71d3a8b580b2f0150561fba27
joao <jv.dossantos012@gmail.com>
2024-06-29 17:31:34 -0300

    init project

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,27 @@
+import { AnswersRepository } from './../repositories/answer-repository';
+import { Answer } from "../entities/answer"
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string
+  questionId: string
+  content: string
+}
+
+export class AnswerQuestionUseCase {
+  constructor(
+    private answersRepository: AnswersRepository
+  ) {}
+  
+
+  async execute ({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer({
+      content, 
+      authorId: instructorId, 
+      questionId,
+    })
+
+    await this.answersRepository.create(answer)
+
+    return answer
+  }
+}
