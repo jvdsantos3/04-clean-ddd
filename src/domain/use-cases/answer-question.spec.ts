d945e2655e9071a0868370739ad6f9b027854d66
João <joao.alcantara@fabricainfo.com>
2025-06-21 17:08:04 -0300

    DDD

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,21 @@
+import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswerRepository } from "../repositories/answer-repository";
+import { Answer } from "../entities/answer";
+
+const fakeAnswerRepository: AnswerRepository = {
+  create: async (answer: Answer) => {
+    return;
+  },
+};
+
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository);
+
+  const answer = await answerQuestion.execute({
+    questionId: "1",
+    instructorId: "1",
+    content: "Nova resposta",
+  });
+
+  expect(answer.content).toEqual("Nova resposta");
+});
